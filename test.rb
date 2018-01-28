69a55415621e0db17b5cc36caa4930b9c3fa234a
Jamison Ordway <jamisonordway@gmail.com>
2018-01-28 10:01:43 -0600

    Encryptor and Decrypt

---
+++ test.rb
@@ -0,0 +1,36 @@
+class Encryptor
+
+  def cipher(rotation)
+    characters = (' '..'z').to_a
+    rotated_characters = characters.rotate(rotation)
+    Hash[characters.zip(rotated_characters)]
+  end
+
+  def encrypt_letter(letter, rotation)
+    cipher_for_rotation = cipher(rotation)
+    cipher_for_rotation[letter]
+  end
+
+  def reverse_rotation(rotation)
+    rotation * -1
+  end
+
+  def encrypt(string, rotation)
+    letters = string.split("")
+    results = string.split("")
+    results = letters.collect do |letter|
+      encrypt_letter(letter, rotation)
+    end
+    results.join
+  end
+
+  def decrypt(string, rotation)
+    encrypted_letters = string.split("")
+
+    results = encrypted_letters.collect do |letter|
+      encrypt_letter(letter, reverse_rotation(rotation))
+    end
+
+    results.join
+  end
+end
