d252a004c33a30d32d21fd6ef2b03f6e55c5f21a
Jamison Ordway <jamisonordway@gmail.com>
2018-01-28 13:42:23 -0600

    Trying to encrypt just one letter using the rotate mehtod

--- encrypt_letter.rb
+++ encrypt_letter.rb
@@ -1,9 +1,13 @@
 class Encryptor
 
+def cipher
+  characters = (' '..'z').to_a
+  rotated_characters = characters.rotate(rotation)
+  Hash[characters.zip(rotated_characters)]
+end
+
 def encrypt(letter, rotation)
-characters = (' '..'z').to_a
-rotated_characters = characters.rotate("", rotation)
-Hash[characters.zip(rotated_characters)]
+cipher[letter]
 end
 
 
