7c3e8ad33932de7e9502e23b470e5996fe5e0145
Jamison Ordway <jamisonordway@gmail.com>
2018-02-07 08:43:44 -0600

    updated encryptor

--- encryptor.rb
+++ encryptor.rb
@@ -1,20 +1,19 @@
 class Encryptor
 
-  def rotator(letter, number)
-    character_map = (' '..'z').to_a
-    index = character_map.index(letter)
-    character_map.rotate(number)[index]
-  end
+def rotator(letter, number)
+  character_map = (" ".."z").to_a
+  index = character_map.index(letter)
+  character_map.rotate(number)[index]
+end
 
 def decrypt(message, key)
-  message.chars.map do |letter|
-    rotator(letter, key)
-  end.join
-end
+ message.chars.map do |letter|
+   rotator(letter, key)
+ end.join
 
 def encrypt(message, key)
   message.chars.map do |letter|
-    rotator(letter, (key * -1))
+    rotator(letter, (key *-1))
   end.join
- end
+end
 end
